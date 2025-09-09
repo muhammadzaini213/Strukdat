aa2ae57d6d8262c63adce4deae281e8b5bbc8bc3
Andi Naufal Nurfadhil <111062395+andinaufal120@users.noreply.github.com>
2025-09-09 10:26:46 +0800

    add dedicated swap demonstration

--- Queue/Test.java
+++ Queue/Test.java
@@ -18,7 +18,23 @@ public class Test {
         System.out.println(queue.peek(4));
 
         System.out.println("\n=== Dequeue Data ===");
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        System.out.println("=== Mengisi Data ===");
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        System.out.println("\n=== Swap Data ===");
         queue.swap(0,2);
+
+        System.out.println("\n=== Dequeue Data ===");
         System.out.println(queue.dequeue());
         System.out.println(queue.dequeue());
         System.out.println(queue.dequeue());
