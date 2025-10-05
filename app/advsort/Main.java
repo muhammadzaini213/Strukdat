aef64482d264bd318d457729df8800d154eb06f0
muhammadzaini213 <oniichanterritory@gmail.com>
2025-10-06 07:25:30 +0800

    ShellSort

--- app/advsort/Main.java
+++ app/advsort/Main.java
@@ -1,6 +1,7 @@
 package app.advsort;
 
 import amerika.sorting.advancesorting.QuickSort;
+import amerika.sorting.advancesorting.ShellSort;
 import java.util.Arrays;
 
 public class Main {
@@ -12,7 +13,8 @@ public class Main {
     
     public void sort() {
         int[] arr = {2,6,8,48,2,5,4,9,7,0,1,5};
-        QuickSort.quickSort(arr, 0, arr.length - 1);
+        // QuickSort.quickSort(arr, 0, arr.length - 1);
+        ShellSort.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
