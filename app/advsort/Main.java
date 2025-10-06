2ee9f01820244e313f619099bc74e996589928c0
muhammadzaini213 <oniichanterritory@gmail.com>
2025-10-06 21:10:01 +0800

    mergesort

--- app/advsort/Main.java
+++ app/advsort/Main.java
@@ -2,6 +2,7 @@ package app.advsort;
 
 import amerika.sorting.advancesorting.QuickSort;
 import amerika.sorting.advancesorting.ShellSort;
+import amerika.sorting.advancesorting.MergeSort;
 import java.util.Arrays;
 
 public class Main {
@@ -14,7 +15,7 @@ public class Main {
     public void sort() {
         int[] arr = {2,6,8,48,2,5,4,9,7,0,1,5};
         // QuickSort.quickSort(arr, 0, arr.length - 1);
-        ShellSort.sort(arr);
+        MergeSort.mergeSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 }
